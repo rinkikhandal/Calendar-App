4a91ba6484ce99e22f4cb96f0d3137c275fdf361
rinki <rinkikhandal2000@gmail.com>
2023-09-18 21:26:03 +0530

    complete with year also

--- Calendar_App/src/components/YearsCalendar.jsx
+++ Calendar_App/src/components/YearsCalendar.jsx
@@ -1,5 +1,34 @@
+import { useContext, useEffect } from "react";
+import DatesContext from "../context/DatesContext";
+import { Months } from "./Date";
+
 const YearsCalendar = () => {
-  return <div></div>;
+  const { yearCalendar, setYearCalendar, supMonth, setSupMonth, supYear } =
+    useContext(DatesContext);
+
+  useEffect(() => {}, [supMonth, yearCalendar]);
+
+  const handleMonthClick = (index) => {
+    setSupMonth(index);
+    setYearCalendar(false);
+    console.log("clicked month", supMonth, supYear, yearCalendar);
+  };
+
+  return (
+    <div className='year-grid'>
+      {Months.map((month, index) => {
+        return (
+          <span
+            key={index}
+            className='month-hover'
+            onClick={() => handleMonthClick(index)}
+          >
+            {month.slice(0, 3)}
+          </span>
+        );
+      })}
+    </div>
+  );
 };
 
 export default YearsCalendar;
